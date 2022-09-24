5e215ceddc4c5630bfd547e38cf49a482fca20d1
chihyu0917 <selina221947@gmail.com>
2022-09-24 22:58:53 +0800

    Update reader.py

--- reader.py
+++ reader.py
@@ -5,10 +5,6 @@ with open("../201801/vdtrafonetab_info_001.csv", newline = '', encoding='utf-8')
     datasheet = csv.reader(filestream, delimiter=',', quotechar='|')
     for row in datasheet:
         time+=1
-        # if fileroot == row[0]:
-        #     counterpart_number = row[1]
-        #     # print(counterpart_number)
-        #     break
         print(row)
         if time == 5:
             break
