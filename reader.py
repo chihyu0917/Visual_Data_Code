d5c8db286b76d69d663a318a3302ee3fb16de683
chooisio <kelly9209201@gmail.com>
2022-11-28 14:55:38 +0800

    test

--- reader.py
+++ reader.py
@@ -1,10 +1,10 @@
 import csv
 
 time = 0
-with open("../201801/vdtrafonetab_info_001.csv", newline = '', encoding='utf-8') as filestream:
+with open("/Volumes/LaCie/04_111project/data of project/201801/vdtrafonetab_info_001.csv", newline = '', encoding='utf-8') as filestream:
     datasheet = csv.reader(filestream, delimiter=',', quotechar='|')
     for row in datasheet:
         time+=1
         print(row)
-        if time == 5:
+        if time == 20:
             break
